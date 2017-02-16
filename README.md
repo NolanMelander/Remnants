50eee4af98a45e0841d575b309a77d2d942fd486
Nolan Melander <mel09001@byui.edu>
2017-02-16 14:20:13 -0800

    Updated README.md with spelling corrections, and proper links.

--- README.md
+++ README.md
@@ -8,14 +8,18 @@ Team:
 Sponsors:
 + Luke Powell
 
+Current Update (2/16/2017):
+  We have begun modifying of the BludBourne code to be compatible with Remnant. Over the next few days we will begin to remove/change the code so that it
+  works how we want it for Remnant.
+
 Other Info:  
-  This project was done for **CS246** at **BYU-I**.  
-  To be able to create this program in the time allocated we started with the open source code for Bludbourne.
-  Some of these changes included new menus, party system, and battle UI for an entirely different game.
-  Bludbourne is open source as indicated on page **418** of Mastering LibGDX GameDevelopment.  
-  The original BludBourne code can be found on Patrick Hoey's official github [here](https://github.com/patrickhoey/BludBourne)
-  
+  This project was done for **CS246** at **BYU-I**.
+  To be able to create this program in the time allocated we will start with the original code for Bludbourne.
+  Some of the planned changes include new menus, a new party system, and battle UI.
+
 More Info:  
   This project was created using the libgdx library. Found [here](https://libgdx.badlogicgames.com/).  
   Mastering LibGDX Game Development by Patrick Hoey was a big help in this. Find his book [here](https://www.packtpub.com/game-development/mastering-libgdx-game-development).  
-  
+  This project started with code from Bludbourne and several classes were changed and modified.
+  The BludBourne code, in all of it's originality can be found on Patrick Hoey's GitHub [here](https://github.com/patrickhoey/BludBourne)
+  Bludbourne is open source as indicated on page **418** of Mastering LibGDX GameDevelopment.
--- core/src/com/remnants/game/Remnants.java
+++ core/src/com/remnants/game/Remnants.java
@@ -1,33 +1,80 @@
 package com.remnants.game;
 
-import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.Game;
 
-public class Remnants extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
-	
-	@Override
-	public void create () {
-		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+import com.badlogic.gdx.Screen;
+
+import com.remnants.game.screens.CreditScreen;
+import com.remnants.game.screens.CutSceneScreen;
+import com.remnants.game.screens.GameOverScreen;
+import com.remnants.game.screens.LoadGameScreen;
+import com.remnants.game.screens.MainGameScreen;
+import com.remnants.game.screens.MainMenuScreen;
+import com.remnants.game.screens.NewGameScreen;
+
+
+public class Remnants extends Game {
+
+	private static MainGameScreen _mainGameScreen;
+	private static MainMenuScreen _mainMenuScreen;
+	private static LoadGameScreen _loadGameScreen;
+	private static NewGameScreen _newGameScreen;
+	private static GameOverScreen _gameOverScreen;
+	private static CutSceneScreen _cutSceneScreen;
+	private static CreditScreen _creditScreen;
+
+	public static enum ScreenType{
+		MainMenu,
+		MainGame,
+		LoadGame,
+		NewGame,
+		GameOver,
+		WatchIntro,
+		Credits
+	}
+
+	public Screen getScreenType(ScreenType screenType){
+		switch(screenType){
+			case MainMenu:
+				return _mainMenuScreen;
+			case MainGame:
+				return _mainGameScreen;
+			case LoadGame:
+				return _loadGameScreen;
+			case NewGame:
+				return _newGameScreen;
+			case GameOver:
+				return _gameOverScreen;
+			case WatchIntro:
+				return _cutSceneScreen;
+			case Credits:
+				return _creditScreen;
+			default:
+				return _mainMenuScreen;
+		}
+
 	}
 
 	@Override
-	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
-		batch.end();
+	public void create(){
+		_mainGameScreen = new MainGameScreen(this);
+		_mainMenuScreen = new MainMenuScreen(this);
+		_loadGameScreen = new LoadGameScreen(this);
+		_newGameScreen = new NewGameScreen(this);
+		_gameOverScreen = new GameOverScreen(this);
+		_cutSceneScreen = new CutSceneScreen(this);
+		_creditScreen = new CreditScreen(this);
+		setScreen(_mainMenuScreen);
 	}
-	
+
 	@Override
-	public void dispose () {
-		batch.dispose();
-		img.dispose();
+	public void dispose(){
+		_mainGameScreen.dispose();
+		_mainMenuScreen.dispose();
+		_loadGameScreen.dispose();
+		_newGameScreen.dispose();
+		_gameOverScreen.dispose();
+		_creditScreen.dispose();
 	}
+
 }
