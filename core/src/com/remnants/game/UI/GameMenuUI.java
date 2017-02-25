36bce5d328390923805e88827da2c759aa8988e4
codeunifier <brianemdn@gmail.com>
2017-02-25 16:10:17 -0700

    Added battle sprite and resized char. sprites

--- core/src/com/remnants/game/UI/GameMenuUI.java
+++ core/src/com/remnants/game/UI/GameMenuUI.java
@@ -7,14 +7,13 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Container;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
-import com.remnants.game.Entity;
-import com.remnants.game.InputComponent;
 import com.remnants.game.Remnants;
 import com.remnants.game.Utility;
 
@@ -41,7 +40,6 @@ public class GameMenuUI implements Screen {
 
         //button creation
         TextButton backButton = new TextButton("Back", Utility.STATUSUI_SKIN);
-
         TextButton spellButton = new TextButton("Spells", Utility.STATUSUI_SKIN);
         TextButton armorButton = new TextButton("Armor", Utility.STATUSUI_SKIN);
         TextButton weaponButton = new TextButton("Weapons", Utility.STATUSUI_SKIN);
@@ -60,14 +58,26 @@ public class GameMenuUI implements Screen {
         Image spellBook2 = new Image(new TextureRegionDrawable(new TextureRegion(new Texture("skins/temp/flame-scroll.png"))));
         Image armor = new Image(new TextureRegionDrawable(new TextureRegion(new Texture("skins/temp/yellow-tunic-plain.png"))));
         Image weapon = new Image(new TextureRegionDrawable(new TextureRegion(new Texture("skins/temp/broad-sword.png"))));
+        Image battleSprite = new Image(new TextureRegionDrawable(new TextureRegion(new Texture("skins/temp/abella-battle-sprite.png"))));
 
         //back button
         backButton.setWidth(buttonWidth);
         backButton.setHeight(buttonHeight);
-        backButton.setPosition(_stage.getWidth() *.06f, _stage.getHeight() *.8f);
+        backButton.setPosition(0, _stage.getHeight() - buttonHeight);
+
+        //battle sprite
+        battleSprite.setHeight(_stage.getHeight() / 2);
+        battleSprite.setWidth(_stage.getHeight() / 2);
+        battleSprite.setPosition(buttonWidth / 2, buttonHeight * 2);
+
+        //set sprites to fill button square
+        tarenSprite.getImageCell().expand().fill();
+        abellaSprite.getImageCell().expand().fill();
+        ipoSprite.getImageCell().expand().fill();
+        tyrusSprite.getImageCell().expand().fill();
 
         //table layout
-        table.setDebug(true);
+        //table.setDebug(true);
         table.top().right();
         table.add(spellBook1).width(buttonHeight).height(buttonHeight);
         table.add(spellBook2).width(buttonHeight).height(buttonHeight);
@@ -94,7 +104,7 @@ public class GameMenuUI implements Screen {
         float spritePadding = buttonHeight - (buttonHeight * 0.15f);
 
         //sprite table layout
-        spriteTable.setDebug(true);
+        //spriteTable.setDebug(true);
         spriteTable.left().bottom();
         spriteTable.padBottom(buttonHeight/2).padLeft(spritePadding);
         spriteTable.add(tarenSprite).width(buttonHeight).height(buttonHeight).padRight(spritePadding);
@@ -102,42 +112,35 @@ public class GameMenuUI implements Screen {
         spriteTable.add(ipoSprite).width(buttonHeight).height(buttonHeight).padRight(spritePadding);
         spriteTable.add(tyrusSprite).width(buttonHeight).height(buttonHeight).padRight(spritePadding);
 
+        _stage.addActor(battleSprite);
         _stage.addActor(backButton);
         _stage.addActor(table);
         _stage.addActor(spriteTable);
 
-        Gdx.app.log(TAG,"Creating button listener");
         //Button Listeners
         spellButton.addListener(new ClickListener() {
                                     @Override
                                     public boolean touchDown(InputEvent event, float x, float y, int pointer, int button) {
-                                        Gdx.app.log(TAG,"spell button touched down");
+
                                         return true;
                                     }
 
                                     @Override
                                     public void touchUp (InputEvent event, float x, float y, int pointer, int button) {
-                                        Gdx.app.log(TAG, "spell button clicked");
+
                                         //_game.setScreen(_game.getScreenType(Remnants.ScreenType.SpellScreen));
                                     }
-                                }
-        );
-    }
 
-    @Override
-    public void dispose() {
+
+        });
 
     }
 
     @Override
-    public void hide() {
-
-    }
+    public void dispose() {}
 
     @Override
-    public void pause() {
-
-    }
+    public void pause() {}
 
     @Override
     public void render(float delta) {
@@ -152,17 +155,15 @@ public class GameMenuUI implements Screen {
     }
 
     @Override
-    public void resize(int width, int height) {
-
-    }
+    public void resize(int width, int height) {}
 
     @Override
-    public void resume() {
-
-    }
+    public void resume() {}
 
     @Override
-    public void show() {
+    public void show() {}
 
-    }
+    @Override
+    public void hide() {}
+
 }
